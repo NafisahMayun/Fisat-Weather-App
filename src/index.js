33ac85572feccf12ab3f055dff1494b9fed971a9
Nafisah Mayun <146647721+NafisahMayun@users.noreply.github.com>
2023-11-30 05:51:56 -0800

    added temperature and refresh weather functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+    let apiKey = "50452302tb37b4od98a0358fbcba378a";
+    let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric';
+console.log(apiUrl)
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#form-input");
@@ -7,3 +22,5 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
